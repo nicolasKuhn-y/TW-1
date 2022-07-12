ba1ae529438be5f20c1c23a7137ed72b6bd4975b
Nicolas Kuhn <nicolas.kuhn@neoris.com>
2022-07-11 22:24:07 -0300

    Refactor

--- src/main/java/ar/edu/unlam/tallerweb1/controladores/VaccineController.java
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/VaccineController.java
@@ -10,40 +10,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Controller
 public class VaccineController {
 
     private IVaccineService vaccineService;
+
     @Autowired
-    public VaccineController(IVaccineService vaccineService){
-        this.vaccineService=vaccineService;
+    public VaccineController(IVaccineService vaccineService) {
+        this.vaccineService = vaccineService;
     }
 
-    @RequestMapping(value="/vaccine-recommended-by-age",method = RequestMethod.GET)
-    public ModelAndView listVaccineRecommendedByAge(@RequestParam(value = "anio", required = false) Integer anio){
+    @RequestMapping(value = "/vaccine-recommended-by-age", method = RequestMethod.GET)
+    public ModelAndView listVaccineRecommendedByAge(@RequestParam(value = "anio", required = false) Integer anio) {
         ModelMap model = new ModelMap();
         try {
             List<Vaccine> listRecommended = vaccineService.findVaccinesRecommendedByAge(anio);
-            model.put("vaccine",listRecommended);
-        }catch (Exception e){
-            model.put("error","No se recomiendan vacunas para su edad");
+            model.put("vaccine", listRecommended);
+        } catch (Exception e) {
+            model.put("error", "No se recomiendan vacunas para su edad");
         }
-        return new ModelAndView("/vaccine-recommended-by-age",model);
+        return new ModelAndView("/vaccine-recommended-by-age", model);
     }
 
     @RequestMapping("/all-vaccine")
-    public ModelAndView listAllVaccine(){
+    public ModelAndView listAllVaccine() {
         ModelMap model = new ModelMap();
-        try{
-            List<Vaccine>  listVaccine = vaccineService.findAllVaccine();
-            model.put("vaccine",listVaccine);
-        }catch (Exception e){
-            model.put("error","No se encontraron vacunas en la base de datos");
+        try {
+            List<Vaccine> listVaccine = vaccineService.findAllVaccine();
+            model.put("vaccine", listVaccine);
+        } catch (Exception e) {
+            model.put("error", "No se encontraron vacunas en la base de datos");
         }
-        return new ModelAndView("/all-vaccine",model);
+        return new ModelAndView("/all-vaccine", model);
     }
 }
